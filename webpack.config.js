a1faf3c088fc52db8dc1bcfb255b9f1f8164b6a5
zhentaoo <1904974327@qq.com>
2017-02-16 15:42:53 +0800

    add something

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 module.exports = {
   entry: {
-    index: ['./index.js'],
+    index: ['./src/index.js'],
   },
   output: {
     path: path.resolve(__dirname, 'build'),
